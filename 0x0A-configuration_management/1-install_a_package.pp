8ecdcfd27ac404f16f68d240bbbcd97d2b1de194
Fortune Iheanacho <nachodev369@gmail.com>
2024-06-29 03:41:28 +0100

    Task 1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#Install flask version 2.1.0
+
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
 }
+
+exec {'python-installed':
+command => '/u
